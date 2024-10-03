99bbe90ed2395531f8605b6cd94b7198e5b38457
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-10-03 12:31:30 -0300

    Update and rename styles.ts to index.tsx

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CabecalhoStyled } from './cabecalho'
+
+const Cabecalho = () => (
+  <CabecalhoStyled>
+    <h1>EBAC Jobs</h1>
+  </CabecalhoStyled>
+)
+
+export default Cabecalho
