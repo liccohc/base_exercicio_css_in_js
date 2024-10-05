36fd7cf18d8a9fa9d4df9e623c080e839770443b
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-10-05 19:28:45 -0300

    Delete src/components/Cabecalho.tsx

--- src/components/Cabecalho.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const CabecalhoStyled = styled.header`
-  background-color: ${({ theme }) => theme.corSecundaria};
-  color: ${({ theme }) => theme.corPrincipal};
-  text-align: center;
-  padding: 24px 0;
-`
