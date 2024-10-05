75858b06eccdcf90102c1462a387aff63c48d827
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-10-05 20:09:58 -0300

    Update Cabecalho.tsx

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
 
-export const CabecalhoStyled = styled.header`
+const CabecalhoStyled = styled.header`
   background-color: ${({ theme }) => theme.corSecundaria};
   color: ${({ theme }) => theme.corPrincipal};
   text-align: center;
   padding: 24px 0;
 `
+
+const Cabecalho = () => (
+  <CabecalhoStyled>
+    <h1>EBAC Jobs</h1>
+  </CabecalhoStyled>
+)
+
+export default Cabecalho
