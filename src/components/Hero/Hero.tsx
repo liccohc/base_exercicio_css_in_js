06dbb4fb34f929ec16ce9775d30dd411f5f89205
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-10-05 20:12:18 -0300

    Update and rename HeroStyled.tsx to Hero.tsx

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+const HeroStyled = styled.div`
+  height: 360px;
+  width: 100%;
+  background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
+  background-size: cover;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${({ theme }) => theme.corPrincipal};
+    content: '';
+    opacity: 0.7;
+  }
+`
+
+const TitleStyled = styled.h2`
+  position: relative;
+  color: #eee;
+  font-family: 'Gloock', serif;
+  font-size: 48px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+  }
+`
+
+const Hero = () => (
+  <HeroStyled>
+    <div className="container">
+      <TitleStyled>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </TitleStyled>
+    </div>
+  </HeroStyled>
+)
+
+export default Hero
