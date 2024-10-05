60400f75e85069607285c422ae26ada57c847528
Pedro Xavier <155593419+liccohc@users.noreply.github.com>
2024-10-05 19:57:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../src/global'
+import { theme } from './theme'
 import Header from './components/Cabecalho'
-import { Hero } from './components/Hero/HeroStyled'
-import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import Hero from './components/Hero'
+import ListaVagas from './containers/ListaVaga'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
